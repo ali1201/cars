e2d54c9bc14e715aa66c9e0ce17854f3cd8e22ad
Fares Abousaif <faresabousaif@gmail.com>
2021-06-25 07:02:43 +0300

    Call the methods on the class instance.

--- cars/Program.cs
+++ cars/Program.cs
@@ -6,33 +6,34 @@ namespace cars
   {
     static void Main(string[] args)
     {
-
-
       Console.WriteLine("----------------------------------");
 
       Bmw a = new Bmw("sudan", "2020", "black");
       Console.WriteLine(a.type + "\n" + a.model + "\n" + a.color);
-      Car.Testing();
-      Car.Starting();
+      //Car.Testing();
+      //Car.Starting();
+      a.Starting();
       Console.WriteLine("----------------------------------");
+
       Bmw b = new Bmw("Xfor", "2021", "red");
       Console.WriteLine(b.type + "\n" + b.model + "\n" + b.color);
-
-
-      Car.Testing();
-      Car.Starting();
+      //Car.Testing();
+      //Car.Starting();
+      b.Starting();
       Console.WriteLine("------------------------------");
 
       Lexus c = new Lexus("460L", "2018", "selfr");
       Console.WriteLine(c.type + "\n" + c.model + "\n" + c.color);
-      Car.Testing();
-      Car.Starting();
+      //Car.Testing();
+      //Car.Starting();
+      c.Starting();
       Console.WriteLine("------------------------------");
 
       Lexus f = new Lexus("jeep", "2022", "blue");
       Console.WriteLine(f.type + "\n" + f.model + "\n" + f.color);
-      Car.Testing();
-      Car.Starting();
+      //Car.Testing();
+      //Car.Starting();
+      f.Starting();
 
     }
   }
